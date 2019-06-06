3596b237232a4139935791f323476d9cd8f4f54b
unknown <bpro249@gmail.com>
2019-06-06 21:04:07 +0530

    created proper procfile

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     SECRET_KEY = 'jiskibiwimotiuskabhibaranamhai'
     SESSION_COOKIE_SECURE = True
     SQLALCHEMY_TRACK_MODIFICATIONS =False
-    SQLALCHEMY_DATABASE_URI  = 'postgresql://postgres:1234@localhost:5432/askprocoders'
+    SQLALCHEMY_DATABASE_URI  = 'postgres://mpwwktyfviknny:845c95a5b9988217222dab1975efa921308abbab8011fdb737e9bc42f164a70e@ec2-107-20-230-70.compute-1.amazonaws.com:5432/d5h0s3qu502euh'
 
 class ProductionConfig(Config):
     pass
