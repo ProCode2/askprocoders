8c736c3b46ca2feae6902b92876ee8d9b03b853a
unknown <bpro249@gmail.com>
2019-06-12 13:30:21 +0530

    v2

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template,redirect,url_for,request
-app.config.from_object("config.ProductionConfig")
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField
@@ -11,6 +10,9 @@ from sqlalchemy import update
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import LoginManager,UserMixin,login_user,login_required,logout_user,current_user
 
+
+app = Flask(__name__)
+app.config.from_object("config.ProductionConfig")
 Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
