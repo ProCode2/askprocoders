86acdb7f5eabb5e2b8e106e1606c10ef3d3efdd4
procoder249 <50803990+procoder249@users.noreply.github.com>
2019-08-20 17:23:15 +0530

    Commented code

--- app.py
+++ app.py
@@ -10,18 +10,28 @@ from sqlalchemy import update
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import LoginManager,UserMixin,login_user,login_required,logout_user,current_user
 
-
+#initialising flask app
 app = Flask(__name__)
+
+#fetching Environment configs from config.py
 app.config.from_object("config.ProductionConfig")
+
+
 Bootstrap(app)
+
+#creating sqlalchemy instance for creating tables in the database 
 db = SQLAlchemy(app)
+
+#instance of flask login manager
 login_manager = LoginManager()
+
+#Wrapping app with login manager to handle login logouts
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-
+#class to check if every username is unique or not
 class Unique(object):
     def __init__(self, model, field, message):
         self.model = model
@@ -34,7 +44,7 @@ class Unique(object):
             raise ValidationError(self.message)
 
 
-
+#class to create "users" table in the database
 class users(UserMixin,db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(16), unique=True, nullable=False)
@@ -42,7 +52,7 @@ class users(UserMixin,db.Model):
 	password = db.Column(db.String(80), nullable=False)
 	answered = db.Column(db.Integer, nullable=True)
 
-
+#clas to create "qa" table to store question and answer in the database
 class qa(db.Model):
 	sno = db.Column(db.Integer, primary_key=True)
 	id = db.Column(db.BigInteger, nullable=False)
@@ -56,11 +66,13 @@ class qa(db.Model):
 def loaduser(user_id):
 	return users.query.get(int(user_id))
 
+#Login form back-end workings
 class LoginForm(FlaskForm):
 	username = StringField('username' , validators = [InputRequired(),Length(min = 4 ,max=16)])
 	password = PasswordField('password' , validators = [InputRequired() , Length(min = 6, max = 80)])
 	remember = BooleanField('remember me')
 
+# Sign-in or Registration form back-end workings
 class RegistrationForm(FlaskForm):
 	email = StringField('email' , validators = [InputRequired() , Email('Invalid email') , Length(max = 50) , Unique( users,users.email,message='There is already an account with that email.')])
 	username = StringField('username' , validators = [InputRequired(),Length(min = 4 ,max=16) ,  Unique( users,users.username,message=''' Don't you wanna be a bit DIFFERENT from others!  ''')])
@@ -68,11 +80,12 @@ class RegistrationForm(FlaskForm):
 
 
 
-
+#Home page view routing/rendering
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#Login page view
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -88,6 +101,7 @@ def login():
     return render_template('login.html', form=form)
 
 
+#Signup page view
 @app.route('/signup', methods = ['GET' , 'POST'])
 def signup():
 	form = RegistrationForm()
@@ -97,6 +111,8 @@ def signup():
 		db.session.add(new_user)
 		db.session.commit()
 	return render_template('signup.html' , form = form)
+
+#dashboard view
 @app.route('/dashboard' , methods = ['GET' , 'POST'])
 @login_required
 def dashboard():
@@ -111,18 +127,23 @@ def dashboard():
 		searched_posts=posts
 	return render_template('dashboard.html' , name = current_user.username , posts =searched_posts)
 
+#user logout handler
 @app.route('/logout')
 @login_required
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
 
+#Answer View
 @app.route('/answer/<int:post_id>' , methods = ['GET'])
 @login_required
 def answer(post_id):
 	post = qa.query.filter_by(sno = post_id).first()
 	return render_template('answer.html' , post = post)
 
+
+#storing answer in the database
+#answer page view
 @app.route('/submit' , methods = ['POST'])
 @login_required
 def answ():
@@ -139,11 +160,14 @@ def answ():
 	db.session.commit()
 	return render_template('submit.html' , ans = answ , id = id)
 
-
+#404 not found error handler
 @app.errorhandler(404)
 def not_found(e):
 	return render_template('404.html')
 
+#main function
+#app runner
+#creating all databases
 if __name__ == '__main__':
     db.create_all()
     port = int(os.environ.get("PORT", 5000))
